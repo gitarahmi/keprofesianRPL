4ce038a3ab8dc42970886ec87464f1670bb3a91e
gitarahmi <100848589+gitarahmi@users.noreply.github.com>
2023-04-03 11:59:17 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,13 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        // assignment
         int number = 10;
 
-
+        // perulangan
         for (int i = 1; i <= number; i++)
         {
-
+            // pemilihan
             if (i % 2 == 0)
             {
                 Console.WriteLine("{0} adalah bilangan genap", i);
